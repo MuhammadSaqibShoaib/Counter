195bef8b70e3e01d858ca3d77305d0bf8d6698da
MuhammadSaqibShoaib <muhammadsaqib54@outlook.com>
2022-12-13 22:36:07 +0500

    Make counter a preference

--- app/src/main/java/com/example/counter/MainActivity.java
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.counter;
 
+import androidx.annotation.RequiresPermission;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,12 +16,15 @@ TextView counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         // setting values from activity main . xml
         counter = findViewById(R.id.counter);/*
         inc_btn = findViewById(R.id.btn_inc);
         dec_btn = findViewById(R.id.btn_dec);
         res_btn = findViewById(R.id.btn_res);*/
+
+        ReadData();
     }
     public void Increment(View v){
         int count = Integer.parseInt(counter.getText().toString());
@@ -39,4 +45,40 @@ TextView counter;
         counter.setText(Integer.toString(count));
     }
 
+    public void ReadData(){
+        SharedPreferences sh = getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
+
+        String s1 = sh.getString("name", "0");
+
+// We can then use the data
+        counter.setText(s1);
+
+    }
+
+
+    public void SaveData(){
+
+// Storing data into SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+
+// Creating an Editor object to edit(write to the file)
+        SharedPreferences.Editor myEdit = sharedPreferences.edit();
+
+// Storing the key and its value as the data fetched from edittext
+        myEdit.putString("name", counter.getText().toString());
+
+        myEdit.commit();
+    }
+
+    @Override
+    protected void onStart() {
+        ReadData();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        SaveData();
+        super.onStop();
+    }
 }
